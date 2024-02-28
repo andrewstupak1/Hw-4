2ad87ed627a3b6aff9c4512660e661504dd53895
Andrew Stupak <andrew.stupak@kellogg.northwestern.edu>
2024-02-28 01:12:27 +0000

    updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    if User.find_by({ "email" => params["email"] }) == nil
+    user = User.find_by({ "email" => params["email"] })
+    if user == nil
       @user = User.new
       @user["username"] = params["username"]
       @user["email"] = params["email"]
       @user["password"] = BCrypt::Password.create(params["password"])
       @user.save
-     redirect_to "/login"
-    
+      redirect_to "/login"
     else
       flash["notice"] = "Email taken."
       redirect_to "/users/new"
