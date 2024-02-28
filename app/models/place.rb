439086a3e18a4a2666d56bc22ee7a91d04a7616f
Andrew Stupak <andrew.stupak@kellogg.northwestern.edu>
2024-02-28 23:10:13 +0000

    commit

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,3 @@
 class Place < ApplicationRecord
-  belongs_to :user
-  has_many :entries
 end
 
-#added while debugging - read online 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ApplicationRecord
-  has_many :places
 end
 
-#added while debugging - read online 
