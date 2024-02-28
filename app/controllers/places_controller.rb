fc05fbf37d0e6bf3b9c164b81cf6ae5c55f5deec
Andrew Stupak <andrew.stupak@kellogg.northwestern.edu>
2024-02-28 23:22:04 +0000

    8

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,19 +5,25 @@ class PlacesController < ApplicationController
     end
   
     def show
-      @place = current_user.places.find_by({ "id" => params["id"] })
-      @entries = Entry.where({ "place_id" => @place["id"] })
+      @place = current_user.places.find_by(id: params[:id])
+      if @place
+        @entries = Entry.where(place_id: @place.id)
+      else
+        redirect_to places_path, alert: "Place not found."
+      end
     end
 
   def new
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    @place = current_user.places.new(place_params) # This automatically sets the user_id for the place
+    if @place.save
+      redirect_to "/places", notice: "Place was successfully created."
+    else
+      render :new
+    end
   end
 end
-
+  #above taken from chat gpt
 
