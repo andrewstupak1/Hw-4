d94b3eb12ff1a2e27d3c72d720dfd6ed4b021afd
Andrew Stupak <andrew.stupak@kellogg.northwestern.edu>
2024-02-28 23:29:41 +0000

    8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
 
+  def authenticate_user!
+    redirect_to login_path, alert: "You must be logged in to access this page." unless current_user
+  end
+  
+  #pulled when debugging ^
+
   def current_user
     # puts "------------------ code before every request ------------------"
     @current_user = User.find_by({ "id" => session["user_id"] })
