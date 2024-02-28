c9d96d5bb756ccac7c8eb8eb242824d7349f9196
Andrew Stupak <andrew.stupak@kellogg.northwestern.edu>
2024-02-28 00:17:58 +0000

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+
+  # Custom route for landing page (aka root route)
+  get "/", :controller => "places", :action => "index"
 end
