49e2fc7ec224a005ba80569f8a52d08d4f186566
Andrew Stupak <andrew.stupak@kellogg.northwestern.edu>
2024-03-01 02:11:02 +0000

    commit vf

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,17 +1,15 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!
 
     def index
       @places = Place.where({ "user_id" => @current_user["id"] })
     end
   
     def show
-      place = Place.find_by({ "id" => params["id"] })
-           if @place
+      @place = Place.find_by({ "id" => params["id"] })
+      if @place
         @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
-        redirect_to :action => 'show', :entries => @entries
-            else
-        redirect_to places_path, alert: "Place not found."
+      else
+        redirect_to "/places", notice: "Place not found."
       end
     end
 
